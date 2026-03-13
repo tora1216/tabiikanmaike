624473d2975d767c7b2e5f5f977d9160d07c65f1
tora1216 <yuusuke121616@icloud.com>
2026-03-13 15:34:52 +0900

    設定の追加

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
   // create folders with index.html instead of .html files; this works better
   // with GitHub Pages' directory-based routing.
   trailingSlash: true,
+  basePath: process.env.NODE_ENV === "production" ? "/tabiikanmaike" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/tabiikanmaike" : "",
 };
 
 export default nextConfig;
