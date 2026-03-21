89c8caa6d457c4464b34554563c5824bfac5f845
tora1216 <yuusuke121616@icloud.com>
2026-03-21 23:35:06 +0900

    ビルドエラーの解消

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function manifest(): MetadataRoute.Manifest {
   const base =
     process.env.VERCEL ? "" : process.env.NODE_ENV === "production" ? "/tabiikanmaike" : "";
